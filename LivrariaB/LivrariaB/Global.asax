431b5458917aaeafa4f6894b1396fbfb98b8134b
biancaquintaes <bianca.quintaes@catolicasc.org.br>
2017-08-29 19:38:17 -0300

    CommitInit

---
+++ LivrariaB/LivrariaB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LivrariaB.MvcApplication" Language="C#" %>
---
+++ LivrariaB/LivrariaB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LivrariaB.Startup))]
+namespace LivrariaB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
